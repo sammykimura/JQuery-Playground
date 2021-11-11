2c303c9ca9526bc74fcf93d5ed3f05b5ab15e01a
sammykimura <91994204+sammykimura@users.noreply.github.com>
2021-11-11 16:19:00 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.0.js.webarchive';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+import $ from "jquery";
+
+$(document).ready(function(){
+
+  $("button").click(function(){
+    $(".rectangles").hide();
+  });
+
+  $("#box2").hover(function(){
+    console.log("You entered box 2-position changed!");
+    $("box2").toggleClass("moveRight");
+  });
+
+  $("#box3").click(function(){
+    console.log("You clicked box 2- color changed!");
+    $("box3").toggleClass("changeColor");
+  });
+
+
+}
